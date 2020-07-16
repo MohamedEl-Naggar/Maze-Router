a6ff21b6054ea6c9021d3ff12c386fee1df75c6c
MohamedEl-Naggar <52180050+MohamedEl-Naggar@users.noreply.github.com>
2020-07-17 01:53:13 +0200

    Update Visualizer.py

--- Visualizer.py
+++ Visualizer.py
@@ -4,13 +4,20 @@ fig, axes = plt.subplots(1,2, figsize = (15,6))
 
 x_coordinates = [1, 1, 3, 3, 5] 
 y_coordinates = [2, 5, 5, 10, 10]
-axes[0].scatter(x_coordinates,y_coordinates)
-axes[0].plot(x_coordinates,y_coordinates)
+x_plot = [i+0.5 for i in x_coordinates]
+y_plot = [i+0.5 for i in y_coordinates]
+
+#axes[0].scatter(x_coordinates,y_coordinates)
+axes[0].scatter(x_plot, y_plot, s=100, c='red', marker='s')
+#axes[0].plot(x_coordinates,y_coordinates)
 
 x_coordinates = [10, 10, 30] 
 y_coordinates = [20, 5, 5]
-axes[0].scatter(x_coordinates,y_coordinates)
-axes[0].plot(x_coordinates,y_coordinates)
+x_plot = [i+0.5 for i in x_coordinates]
+y_plot = [i+0.5 for i in y_coordinates]
+#axes[0].scatter(x_coordinates,y_coordinates)
+axes[0].scatter(x_plot, y_plot, s=100, c='red', marker='s')
+#axes[0].plot(x_coordinates,y_coordinates)
 
 axes[0].set_xticks((range(1,31)))
 axes[0].set_yticks((range(1,31)))
@@ -23,4 +30,3 @@ axes[1].set_title('layer 3 & 4')
 
 fig.tight_layout()
 plt.show()
-
