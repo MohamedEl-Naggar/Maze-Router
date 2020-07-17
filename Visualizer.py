3470a66a7979d586b53e8e796e6004b9ce00674f
MohamedEl-Naggar <52180050+MohamedEl-Naggar@users.noreply.github.com>
2020-07-17 11:48:18 +0200

    Update Visualizer.py

--- Visualizer.py
+++ Visualizer.py
@@ -1,22 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+f = open("out.txt", "r")
+layers = []
+x_coordinates = []
+y_coordinates = []
+counter = 0
+flag = False
+temp = ""
+for x in f:
+    for letter in x:
+        if letter == '(':
+            flag = True
+            counter = 0
+            continue
+        if letter == ')':
+            flag = False
+            y_coordinates.append(temp)
+            temp = ""
+        if(flag):
+            if letter.isdigit():
+                temp += letter
+                continue
+            elif letter == ',':
+                counter +=1
+                if(counter == 1):
+                    layers.append(temp)
+                if(counter == 2):
+                    x_coordinates.append(temp)
+                temp = ""
+
+
 fig, axes = plt.subplots(1,2, figsize = (15,6))
 
-x_coordinates = [1, 1, 3, 3, 5] 
-y_coordinates = [2, 5, 5, 10, 10]
-x_plot = [i+0.5 for i in x_coordinates]
-y_plot = [i+0.5 for i in y_coordinates]
+x_plot = [int(i)+0.5 for i in x_coordinates]
+y_plot = [int(i)+0.5 for i in y_coordinates]
+for i in range(len(x_plot)):
+    if layers[i] == '1':
+        axes[0].scatter(x_plot[i], y_plot[i], s=110, c='blue', marker='s')
+    elif layers[i] == '2':
+        axes[0].scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='s')
+
+
 
 #axes[0].scatter(x_coordinates,y_coordinates)
-axes[0].scatter(x_plot, y_plot, s=100, c='red', marker='s')
-#axes[0].plot(x_coordinates,y_coordinates)
-
-x_coordinates = [10, 10, 30] 
-y_coordinates = [20, 5, 5]
-x_plot = [i+0.5 for i in x_coordinates]
-y_plot = [i+0.5 for i in y_coordinates]
-#axes[0].scatter(x_coordinates,y_coordinates)
-axes[0].scatter(x_plot, y_plot, s=100, c='red', marker='s')
 #axes[0].plot(x_coordinates,y_coordinates)
 
 axes[0].set_xticks((range(1,31)))
