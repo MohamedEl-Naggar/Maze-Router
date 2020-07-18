a3c2e6a4b0284f85a5bea5a8b658f7489242e350
MohamedEl-Naggar <52180050+MohamedEl-Naggar@users.noreply.github.com>
2020-07-18 18:32:53 +0200

    Update Visualizer.py
    Finalized

--- Visualizer.py
+++ Visualizer.py
@@ -29,7 +29,7 @@ for x in f: # search each line in the file
                 counter +=1
                 if counter == 1:
                     layers.append(temp) # cell layer before the first ','
-                    if layers_temp.isdigit() and layers_temp != temp:
+                    if layers_temp.isdigit() and layers_temp != temp: # detect via
                         vias.append(1) 
                     else:
                         vias.append(0)
@@ -50,19 +50,19 @@ for i in range(len(x_plot)):
     if layers[i] == '1':
         axes12.scatter(x_plot[i], y_plot[i], s=110, c='blue', marker='s') # layer 1 is drawn in blue
         if vias[i] == 1:
-            axes12.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='x') 
+            axes12.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='x') # via
     elif layers[i] == '2':
         axes12.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='s') # layer 2 is drawn in purple
         axes23.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='s') # layer 2 is drawn in purple
         if vias[i] == 1:
             if layers[i-1] == '1':
-                axes12.scatter(x_plot[i], y_plot[i], s=110, c='blue', marker='x') 
+                axes12.scatter(x_plot[i], y_plot[i], s=110, c='blue', marker='x') # via
             else:
-                axes23.scatter(x_plot[i], y_plot[i], s=110, c='yellow', marker='x') 
+                axes23.scatter(x_plot[i], y_plot[i], s=110, c='yellow', marker='x') # via
     elif layers[i] == '3':
         axes23.scatter(x_plot[i], y_plot[i], s=110, c='yellow', marker='s') # layer 3 is drawn in yellow
         if vias[i] == 1:
-            axes23.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='x') 
+            axes23.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='x') # via
 
 # axes range
 axes12.set_xticks((range(1,51))) 
@@ -78,5 +78,3 @@ axes23.grid(True)
 axes23.set_title('layer 2 & 3')
 fig2.tight_layout()
 plt.show()
-
-print (vias)
