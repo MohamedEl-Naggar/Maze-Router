d1d435943b44865051e22bf8a2f8d20157edd9b0
MohamedEl-Naggar <52180050+MohamedEl-Naggar@users.noreply.github.com>
2020-07-19 05:59:44 +0200

    Update Visualizer.py

--- Visualizer.py
+++ Visualizer.py
@@ -5,11 +5,9 @@ f = open("out.txt", "r")
 layers = [] 
 x_coordinates = [] # x coordinates array
 y_coordinates = [] # y coordinates array
-vias = []
 counter = 0
 flag = False
 temp = ""
-layers_temp = ""
 for x in f: # search each line in the file
     layers_temp = ""
     for letter in x: # search each letter in the line
@@ -29,10 +27,6 @@ for x in f: # search each line in the file
                 counter +=1
                 if counter == 1:
                     layers.append(temp) # cell layer before the first ','
-                    if layers_temp.isdigit() and layers_temp != temp: # detect via
-                        vias.append(1) 
-                    else:
-                        vias.append(0)
                     layers_temp = temp
                 if counter == 2:
                     x_coordinates.append(temp) # x coordinate before the second ','
@@ -46,23 +40,30 @@ fig2, axes23 = plt.subplots(1,1, figsize = (15,10))
 x_plot = [int(i)+0.5 for i in x_coordinates] 
 y_plot = [int(i)+0.5 for i in y_coordinates]
 
+layers_temp = "" # to check for vias
+x_temp = "" # to check for vias
+y_temp = "" # to check for vias
+
 for i in range(len(x_plot)):
     if layers[i] == '1':
         axes12.scatter(x_plot[i], y_plot[i], s=110, c='blue', marker='s') # layer 1 is drawn in blue
-        if vias[i] == 1:
+        if layers[i] != layers_temp and x_plot[i] == x_temp and y_plot[i] == y_temp: # to check for vias
             axes12.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='x') # via
     elif layers[i] == '2':
         axes12.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='s') # layer 2 is drawn in purple
         axes23.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='s') # layer 2 is drawn in purple
-        if vias[i] == 1:
+        if layers[i] != layers_temp and x_plot[i] == x_temp and y_plot[i] == y_temp: # to check for vias
             if layers[i-1] == '1':
                 axes12.scatter(x_plot[i], y_plot[i], s=110, c='blue', marker='x') # via
             else:
                 axes23.scatter(x_plot[i], y_plot[i], s=110, c='yellow', marker='x') # via
     elif layers[i] == '3':
         axes23.scatter(x_plot[i], y_plot[i], s=110, c='yellow', marker='s') # layer 3 is drawn in yellow
-        if vias[i] == 1:
+        if layers[i] != layers_temp and x_plot[i] == x_temp and y_plot[i] == y_temp: # to check for vias
             axes23.scatter(x_plot[i], y_plot[i], s=110, c='purple', marker='x') # via
+    layers_temp = layers[i]
+    x_temp = x_plot[i]
+    y_temp = y_plot[i]
 
 # axes range
 axes12.set_xticks((range(1,51))) 
